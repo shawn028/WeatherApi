4a69490f8eaf4ec70b1acda561bdde417328b020
shawn028 <shawn028@gmail.com>
2021-02-10 12:34:27 +1100

    Updated

--- src/test/java/runner/ApiRunnerTest.java
+++ src/test/java/runner/ApiRunnerTest.java
@@ -8,12 +8,12 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = ("features"),
-				 glue = ("stepDefinitions"),
-				 plugin = {"html:target/cucumber-reports/html",
-				 		  "json:target/cucumber-reports/cucumber.json",
-						  "junit:target/cucumber-reports/cucumber.xml",
-						  "pretty"}
-)
+glue = ("stepDefinitions"),
+
+plugin = {"html:target/cucumber-reports/html",
+		"json:target/cucumber-reports/cucumber.json",
+		"junit:target/cucumber-reports/cucumber.xml",
+		"pretty"})
 
 public class ApiRunnerTest {
 
